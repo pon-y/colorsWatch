58e7202df3fd788c5ead64624f29a12634ce2f46
Yvonne <>
2019-05-07 21:21:45 -0700

    Initial Commit

---
+++ src/Components/Canvas.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+class Canvas extends Component {
+  constructor ( props) {
+    super ( props );
+  }
+
+  canvasRef = React.createRef();
+
+
+  draw = () => {
+    var canvas = this.canvasRef.current;
+    
+    canvas.addEventListener('click', (e) => {
+
+      const xCoord = e.offsetX;
+      const yCoord = e.offsetY;
+      const canvasWidth = 600;
+      const myImageData = ctx.getImageData(0, 0, 600, 600);
+      const getColorIndicesForCoord = (x, y, width) => {
+        const red = y * (width * 4) + x * 4;
+        return [red, red + 1, red + 2, red + 3];
+      };
+
+      const colorIndices = getColorIndicesForCoord(xCoord, yCoord, canvasWidth);
+
+      const [redIndex, greenIndex, blueIndex, alphaIndex] = colorIndices;
+
+      const colorValues = colorIndices.map(el => myImageData.data[el]);
+
+      const colorpatch = document.getElementById('colorpatch');
+
+      const color = `rgba(${colorValues[0]}, ${colorValues[1]}, ${colorValues[2]}, ${colorValues[3]})`;
+      colorpatch.style.backgroundColor = color;
+      colorpatch.innerHTML = color;
+      console.log(colorValues);
+    });
+
+
+    if (canvas.getContext) {
+     var ctx = canvas.getContext('2d');
+
+      //bakarinaa
+      var img = new Image();
+      img.addEventListener('load', function () {
+        ctx.drawImage( img, 0,0)
+      })
+      img.crossOrigin = "anonymous"
+      img.src = 'https://i1.wp.com/thatnovelcorner.com/wp2/wp-content/uploads/2019/05/102251.jpeg?w=350&ssl=1';
+
+      
+    }
+}
+
+componentDidMount() {
+  this.draw();
+}
+
+
+render () {
+
+  return (
+    <canvas ref={this.canvasRef} id="userImage" width="400" height="400"></canvas>
+    )
+  }
+}
+
+export default Canvas;
