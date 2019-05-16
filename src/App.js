1ed0894b5c60871d8985208b4808b4660b92d2e1
Yvonne <>
2019-05-15 18:17:04 -0700

    Restructure Header and update CSS

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ class App extends Component {
       <div className="App">
 
         <div className="main" >
-        <div className="bigSwatch" style={{backgroundColor: this.state.currentColor}}> ColorSwatch by pon-y <div style={{fontSize: '10px'}}>Click anywhere on the picture to get started</div></div>
+        <Header clickedColorsArray={this.state.clickedColorsArray} currentColor={this.state.currentColor}/>
+        {/* <div className="bigSwatch" style={{backgroundColor: this.state.currentColor}}> ColorSwatch <div style={{fontSize: '10px'}}>Click anywhere on the picture to get started</div></div> */}
         <Canvas onClick={this.clickImageHandler} updateImage={this.updateImage} handleChange={this.handleChange} imgUrl={this.state.imgUrl} urlValue={this.state.urlValue} clickedColorsArray={this.state.clickedColorsArray} deleteItem={this.deleteItem} bigColor={this.bigColor}/> 
         {/* <SwatchList  clickedColorsArray={this.state.clickedColorsArray} deleteItem={this.deleteItem} bigColor={this.bigColor}/> */}
         </div>
@@ -59,3 +60,29 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+function Header ( props ) {
+  
+  const { clickedColorsArray, currentColor } = props;
+  let directionText = 'Click anywhere on the image to begin';
+  if(clickedColorsArray.length > 0) {
+    directionText = null;
+  } 
+  return (
+    <div className="bigSwatch" style={{backgroundColor: props.currentColor}}> 
+      <div style={{color: ~props.currentColor}}>
+        colorSwatch
+      </div>
+      <div>
+       <div style={{fontSize: '15px'}}> 
+        {directionText};
+       </div>
+       <div style={{fontSize: '15px'}}>
+         github.com/pon-y
+       </div>
+      </div>
+    </div>
+  )
+}
