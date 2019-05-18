e72bd007fa7b16f29ec237a0ed58f80ec28401f1
Yvonne <>
2019-05-17 23:03:34 -0700

    Restructure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Canvas from './Components/Canvas.js';
-import SwatchList from './Components/SwatchList.js';
 
 
 class App extends Component {
@@ -9,29 +8,25 @@ class App extends Component {
   state = {
     clickedColorsArray: [],
     currentColor: 'white',
-      urlValue: 'https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2900&q=80',
-      imgUrl: 'https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2900&q=80',
+    urlValue: 'https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2900&q=80',
+    imgUrl: 'https://images.unsplash.com/photo-1464820453369-31d2c0b651af?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2900&q=80',
   }
 
-  //Methods for SwatchList
 
-  //deletes an item from the list
   deleteItem = (idx) => {
     let oldArray = [...this.state.clickedColorsArray];
     oldArray.splice(idx, 1);
     this.setState({clickedColorsArray: oldArray});
   }
 
-  //Methods for Canvas component 
-
-  //Adds a RGBA value from the Canvas component to the array to render to SwatchList
+  
   clickImageHandler = (color) => {
     let colorArray = [...this.state.clickedColorsArray];
     colorArray.push(color);
     this.setState({clickedColorsArray: colorArray});
   }
 
-  //Updates image url 
+ 
   updateImage = () =>  {
     this.setState({imgUrl: this.state.urlValue})
   }
@@ -44,14 +39,14 @@ class App extends Component {
     this.setState({currentColor: color});
   }
   render () {
-
+    const { clickedColorsArray, currentColor, urlValue, imgUrl} = this.state;
     return (
       <div className="App">
 
         <div className="main" >
-        <Header clickedColorsArray={this.state.clickedColorsArray} currentColor={this.state.currentColor}/>
+        <Header clickedColorsArray={clickedColorsArray} currentColor={currentColor}/>
         {/* <div className="bigSwatch" style={{backgroundColor: this.state.currentColor}}> ColorSwatch <div style={{fontSize: '10px'}}>Click anywhere on the picture to get started</div></div> */}
-        <Canvas onClick={this.clickImageHandler} updateImage={this.updateImage} handleChange={this.handleChange} imgUrl={this.state.imgUrl} urlValue={this.state.urlValue} clickedColorsArray={this.state.clickedColorsArray} deleteItem={this.deleteItem} bigColor={this.bigColor}/> 
+        <Canvas onclick={this.clickImageHandler} updateImage={this.updateImage} handleChange={this.handleChange} imgUrl={imgUrl} urlValue={urlValue} clickedColorsArray={clickedColorsArray} deleteItem={this.deleteItem} bigColor={this.bigColor}/> 
         {/* <SwatchList  clickedColorsArray={this.state.clickedColorsArray} deleteItem={this.deleteItem} bigColor={this.bigColor}/> */}
         </div>
     </div>
@@ -65,23 +60,17 @@ export default App;
 
 function Header ( props ) {
   
-  const { clickedColorsArray, currentColor } = props;
-  let directionText = <div>Click anywhere on the image to begin</div>
-  if(clickedColorsArray.length > 0) {
-    directionText = <div style={{opacity: 0}}>Click anywhere on the image to begin</div>;
-  } 
+  const { currentColor } = props;
+  
   return (
-    <div className="bigSwatch" style={{backgroundColor: props.currentColor}}> 
+    <div className="bigSwatch" style={{backgroundColor: currentColor}}> 
       <div className="bigSwatch-label-main">
         illuminate
       </div>
       <div className="bigSwatch-label-sub">
        <div>
-         <a href="https://github.com/pon-y/">github.com/pon-y</a>
+         <a href="https://github.com/pon-y/" target="_blank">github.com/pon-y</a>
        </div>
-       {/* <div>  */}
-        {/* {directionText} */}
-       {/* </div> */}
       </div>
     </div>
   )
